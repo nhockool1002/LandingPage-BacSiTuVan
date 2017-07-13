9d5da7d214199e972e1f801d69d546e3f7981cd0
Kangta <nhut.nguyenminh.it@gmail.com>
2017-07-13 17:17:54 +0700

    Developing ...

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title> Tư Vấn Online </title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="lib/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Anton|Open+Sans+Condensed:300|Roboto+Condensed" rel="stylesheet">
+        <link rel="stylesheet" href="lib/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+    <div class="header">
+      <div class="container-fluid">
+        <div class="col-sm-12">
+          <div class="intro-content">
+            <div class="logo">
+              <img src="img/logo.png">
+          </div>
+          <div class="row">
+            <div class="infopage">
+              <div class="text col-sm-5">
+                  <h1>CHUYÊN TRANG TƯ VẤN BỆNH NAM PHỤ KHOA ONLINE</h1>
+                  <p class="logo-info"> Với hơn 20 năm kinh nghiệm, các bệnh sĩ phòng khám MAYO sẽ tư vấn mọi vấn đề
+                        về Nam Phụ khoa hoàn toàn miễn phí và cách điều trị hiệu quả nhanh chóng, hiệu quả.
+                  </p>
+                  <p class="contact-now">Tư Vấn Ngay</p>
+                  <a href="http://tuvan.mayo.com.vn/lr/chatpre.aspx?id=kuk38256576&lng=en&p=http://dakhoa.mayo.com.vn&e=seo&r=&rf1=https%3A//www.google.com&rf2=.vn/&cid=1499315641952512948071&sid=1499315641952512948071">
+                  <div class="btn btn-success btn-contact">Click here</div>
+                  </a>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-sm-12">
+              <div class="arrow">
+                <a href="#intro"><img src="img/arrow.png" alt=""></a>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a name="intro">
+    <div class="intro">
+      <div class="container-fluid">
+        <div class="col-sm-12">
+          <div class="intro-content">
+          </div>
+        </div>
+      </div>
+    </div>
+    </a>
+    <div class="namkhoa">
+      <div class="container-fluid">
+        <div class="col-sm-12">
+          <div class="intro-content">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="phukhoa">
+      <div class="container-fluid">
+        <div class="col-sm-12">
+          <div class="intro-content">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="benhxahoi">
+      <div class="container-fluid">
+        <div class="col-sm-12">
+          <div class="intro-content">
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
